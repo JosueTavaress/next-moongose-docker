0753e8b31e1a9fd9404863e542af42857dbb4b1c
JosueTavaress <josueferreira8.jf@gmail.com>
2024-05-17 19:42:39 -0300

    frontend (refactor): rename folder

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import TableEmployees from "@/components/table/Table";
 import TEmployees from "@/api/types";
 import mockEmployees from "@/api/employees";
 import Header from "@/components/header/Header";
-import { Container } from '@chakra-ui/react'
+import { Container, Skeleton, SkeletonCircle, SkeletonText, Stack } from '@chakra-ui/react';
 export default function Home() {
   const [employees, setEmployees] = useState<TEmployees[]>([]);
   const [loading, setLoading] = useState(true);
@@ -16,13 +16,19 @@ export default function Home() {
 
   if (loading) {
     return (
-      <p>loading in implementation</p>
+      <Container maxWidth='1500'>
+        <Stack>
+          <Skeleton mt={5} height='70px' />
+          <Skeleton mt={10} height='50px' />
+          {Array(6).fill(null).map((_) => <Skeleton height='50px' />)}
+        </Stack>
+      </Container>
     )
   }
 
   return (
     <Container maxWidth='1500'>
-      <Header/>
+      <Header />
       <TableEmployees employees={employees} setEmployees={() => setEmployees} />
     </Container>
   );

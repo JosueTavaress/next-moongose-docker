c63834b01c84c4d8f36121f7536bcd32d065c0f9
JosueTavaress <josueferreira8.jf@gmail.com>
2024-05-19 01:56:20 -0300

    home (refactor): specific key in map

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,34 +2,41 @@
 import React, { useEffect, useState } from "react";
 import TableEmployees from "@/components/table/Table";
 import TEmployees from "@/api/types";
-import mockEmployees from "@/api/employees";
+import { getEmployee } from "@/api/employees";
 import Header from "@/components/header/Header";
-import { Container, Skeleton, SkeletonCircle, SkeletonText, Stack } from '@chakra-ui/react';
+import { Container, Skeleton, Stack } from '@chakra-ui/react';
+import { useEmployeeContext } from '../context/EmployeeContext';
+
+
 export default function Home() {
-  const [employees, setEmployees] = useState<TEmployees[]>([]);
   const [loading, setLoading] = useState(true);
+  const { employees, setEmployees } = useEmployeeContext();
 
   useEffect(() => {
-    setEmployees(mockEmployees);
-    setLoading(false);
-  }, []);
+    const effectGetEmployee = async () => {
+      const request: TEmployees[] = await getEmployee();
+      setEmployees(request);
+      setLoading(false);
+    }
+    effectGetEmployee();
+  }, [setEmployees]);
 
   if (loading) {
     return (
       <Container maxWidth='1500'>
         <Stack>
-          <Skeleton mt={5} height='70px' />
+          <Header />
           <Skeleton mt={10} height='50px' />
-          {Array(6).fill(null).map((_) => <Skeleton height='50px' />)}
+          {Array(6).fill(null).map((_, idx) => <Skeleton key={idx} height='50px' />)}
         </Stack>
       </Container>
     )
   }
 
   return (
-    <Container maxWidth='1500'>
-      <Header />
-      <TableEmployees employees={employees} setEmployees={() => setEmployees} />
-    </Container>
+      <Container maxWidth='1500'>
+        <Header />
+        <TableEmployees employees={employees} />
+      </Container>
   );
 }
